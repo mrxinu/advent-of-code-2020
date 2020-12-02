fbe166988c914f33c60669c1d51e9b93e603e3ca
Steven Klassen <sklassen@gmail.com>
2020-12-02 14:03:33 -0800

    Brute force what?

--- Day01/main.go
+++ Day01/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
+func main() {
+	amounts, err := readAmounts("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+OUTER:
+	for _, x := range amounts {
+		for _, y := range amounts {
+			if x+y == 2020 {
+				fmt.Println(x * y)
+				break OUTER
+			}
+		}
+	}
+}
+
+func readAmounts(filename string) ([]int, error) {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	amounts := make([]int, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		amount, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+
+		amounts = append(amounts, amount)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return amounts, nil
 }
